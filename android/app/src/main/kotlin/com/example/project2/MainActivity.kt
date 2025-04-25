601097d0d566f11723713d14661fdeadc33feab3
Lenovo <joob7205@gmail.com>
2025-04-25 08:45:47 +0300

    project2_work

--- android/app/src/main/kotlin/com/example/project2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/project2/MainActivity.kt
@@ -1,5 +1,5 @@
-//package com.example.project2
-//
-//import io.flutter.embedding.android.FlutterActivity
-//
-//class MainActivity: FlutterActivity()
+package com.example.project2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
